8e84b4d9a87d489c57b1632edd78c7d72f625b9c
Chris Russell <chris.john.russell@gmail.com>
2018-07-17 15:09:38 +0100

    Add test for viewing statement

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,13 +2,45 @@ require 'account'
 
 describe Account do
 
-  it "takes a date and amount of deposit" do
+  it "completes an account credit" do
     # setup
     account = Account.new
-    # exercise (set date and amount)
-    account.deposit("14/01/2012",1)
-    # verify (a/c date & balance)
-    expect(account.balance).to eq(["14/01/2012",1])
+    # exercise
+    account.credit("10/01/2012", 500)
+    # verify
+    expect(account.transaction[:Date]).to eq("10/01/2012")
+    expect(account.transaction[:Deposit]).to eq(500)
+    expect(account.transaction[:Balance]).to eq(500)
+  end
+
+  it "completes an account withdrawal" do
+    # setup
+    account = Account.new
+    # exercise
+    account.debit("10/01/2012", 500)
+    # verify
+    expect(account.transaction[:Date]).to eq("10/01/2012")
+    expect(account.transaction[:Debit]).to eq(500)
+    expect(account.transaction[:Balance]).to eq(-500)
+  end
+
+  it "changes the account balance" do
+    # setup
+    account = Account.new
+    # exercise
+    account.credit("10/01/2012", 500)
+    account.debit("10/01/2012", 500)
+    # verify
+    expect(account.transaction[:Balance]).to be(0)
+  end
+
+  it "prints an account statement" do
+    # setup
+    account = Account.new
+    # exercise
+    account.credit("10/01/2012", 500)
+    # verify
+    expect(account.statement).to eq([{ :Date => "10/01/2012", :Deposit => 500, :Balance => 500 }])
   end
 
 end
