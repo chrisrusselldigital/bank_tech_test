7f070d695d472336b00d02824e8c79adb961ebea
Chris Russell <chris.john.russell@gmail.com>
2018-07-17 15:10:59 +0100

    Add statement method

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,41 @@
 class Account
 
   def initialize
-    @amount = 0
+    @statement = []
+    @deposits = 0
+    @withdrawals = 0
   end
 
-  def deposit(date, amount)
-    @date = date
-    @amount = amount
-    [@date, @amount]
+  def credit(date, deposit)
+    @deposits = deposit + @deposits
+    @transaction = {
+      Date: date,
+      Deposit: deposit,
+      Balance: (@deposits - @withdrawals)
+    }
+    @statement << @transaction
   end
 
-  def balance
-    [@date, @amount]
+  def debit(date, withdrawal)
+    @withdrawals = withdrawal + @withdrawals
+    @transaction = {
+      Date: date,
+      Debit: withdrawal,
+      Balance: (@deposits - @withdrawals)
+    }
+    @statement << @transaction
+  end
+
+  def transaction
+    @transaction
+  end
+
+  def statement
+    puts ["date || credit || debit || balance"]
+    @statement.reverse_each do |transaction|
+      puts "#{transaction[:Date]} || #{transaction[:Deposit]} || #{transaction[:Debit]} || #{transaction[:Balance]} "
+    end
+
   end
 
 end
