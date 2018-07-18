5bf614988167493efd7c8cdbb9adff6bb3c7727d
Chris Russell <chris.john.russell@gmail.com>
2018-07-18 16:06:29 +0100

    Refactor unit tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,13 +2,6 @@ require 'account'
 
 describe Account do
 
-  it "deposits money into an account" do
-    # setup
-    account = Account.new
-    # exercise & verify
-    expect(account.credit("10/01/2012", 500)).to eq([{ :Date => "10/01/2012", :Deposit => 500, :Balance => 500 }])
-  end
-
   it "withdraws money from an account" do
     # setup
     account = Account.new
@@ -16,6 +9,11 @@ describe Account do
     expect(account.debit("10/01/2012", 500)).to eq([{ :Date => "10/01/2012", :Debit => 500, :Balance => -500 }])
   end
 
-
+  it "deposits money from an account" do
+    # setup
+    account = Account.new
+    # exercise & verify
+    expect(account.credit("10/01/2012", 500)).to eq([{ :Date => "10/01/2012", :Deposit => 500, :Balance => 500 }])
+  end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,16 +9,4 @@ describe Statement do
     expect(statement.header).to eq(["date || credit || debit || balance"])
   end
 
-  it "prints account transactions" do
-    # setup
-    statement = Statement.new
-    # exercise
-    transaction_dbl = double(:date => "10/01/2012", :deposit => 500, :balance => 500)
-    statement.transactions << transaction_dbl
-    # verify
-    expect(transaction_dbl.date).to eq("10/01/2012")
-    expect(transaction_dbl.deposit).to eq(500)
-    expect(transaction_dbl.balance).to eq(500)
-  end
-
 end
