1247e550c5953d4616221c37c455e3f73b3029ee
Chris Russell <chris.john.russell@gmail.com>
2018-07-18 16:39:12 +0100

    Add raise error and tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,4 +16,22 @@ describe Account do
     expect(account.credit("10/01/2012", 500)).to eq([{ :Date => "10/01/2012", :Deposit => 500, :Balance => 500 }])
   end
 
+  it "fails if deposit is less than zero" do
+    # setup
+    account = Account.new
+    # exercise & verify
+    expect { account.credit("10/01/2012", 0) }.to raise_error(RuntimeError, "Deposit needs to be larger than 0.")
+  end
+
+  it "fails if withdrawal is less than zero" do
+    # setup
+    account = Account.new
+    # exercise & verify
+    expect { account.debit("10/01/2012", 0) }.to raise_error(RuntimeError, "Withdrawal needs to be larger than 0.")
+  end
+
+
+
+
+
 end
