6cce39215a25ce5a72c5bb53d7b7b6ecb71eae33
Chris Russell <chris.john.russell@gmail.com>
2018-07-18 13:31:12 +0100

    Move statement test into statement spec

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,16 +16,6 @@ describe Account do
     expect(account.debit("10/01/2012", 500)).to eq([{ :Date => "10/01/2012", :Debit => 500, :Balance => -500 }])
   end
 
-  it "prints account transactions" do
-    # setup
-    statement = Statement.new
-    # exercise
-    transaction_dbl = double(:date => "10/01/2012", :deposit => 500, :balance => 500)
-    statement.transactions << transaction_dbl
-    # verify
-    expect(transaction_dbl.date).to eq("10/01/2012")
-    expect(transaction_dbl.deposit).to eq(500)
-    expect(transaction_dbl.balance).to eq(500)
-  end
+
 
 end
